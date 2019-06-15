c2a2aadbaf6e5d2636cd1822e901fad04073fa1f
zhoubujin <332133281@qq.com>
2019-06-15 12:08:03 +0800

    Update huya.js

--- huya.js
+++ huya.js
@@ -1,7 +1,19 @@
+delay = random(10,20)
+count = 0
 function random(min,max) {
-    return parseInt(Math.random() * (max - min + 1) + min);
+ return parseInt(Math.random() * (max - min + 1) + min);
 }
-
-$("#pub_msg_input").val("aaa")
-$("#msg_send_bt").attr("class","btn-sendMsg enable")
-$("#msg_send_bt").click()
+ 
+ window.setInterval(refreshCount, 1000);
+ function refreshCount() {
+  count++
+  alert(count +" " +delay)
+  if(delay == count){
+   count =0
+   delay = random(10,20)
+   alert("重置"+count +" " +delay)
+   $("#pub_msg_input").val("aaa")
+   $("#msg_send_bt").attr("class","btn-sendMsg enable")
+   $("#msg_send_bt").click()
+  }
+ }

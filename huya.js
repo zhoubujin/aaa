c23a32d3fade5237580ceff48a16fab0c7460ce8
zhoubujin <332133281@qq.com>
2019-06-15 11:18:18 +0800

    Update huya.js

--- huya.js
+++ huya.js
@@ -1,3 +1,7 @@
+function random(min,max) {
+    return parseInt(Math.random() * (max - min + 1) + min);
+}
+
 $("#pub_msg_input").val("aaa")
 $("#msg_send_bt").attr("class","btn-sendMsg enable")
 $("#msg_send_bt").click()
